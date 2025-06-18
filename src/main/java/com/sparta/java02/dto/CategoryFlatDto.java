93640b04a55eb4319c16e354239a8958e912e149
omea92 <thdjun92@naver.com>
2025-06-18 20:53:33 +0900

    init commit

---
+++ src/main/java/com/sparta/java02/dto/CategoryFlatDto.java
@@ -0,0 +1,8 @@
+package com.sparta.java02.dto;
+
+public class CategoryFlatDto {
+
+  private Long id;
+  private String name;
+  private Long parentId;
+}
---
+++ src/main/java/com/sparta/java02/dto/CategoryTreeDto.java
@@ -0,0 +1,10 @@
+package com.sparta.java02.dto;
+
+import java.util.List;
+
+public class CategoryTreeDto {
+
+  private Long id;
+  private String name;
+  List<CategoryTreeDto> children;
+}
