282c66b32741a2d41c271c13c5792bec71a96aa7
himchan92 <omea92@naver.com>
2025-06-25 12:54:35 +0900

    ProductController CRUD API 예제 실습

--- src/main/java/com/sparta/java02/domain/user/dto/UserUpdateStatusRequest.java
+++ src/main/java/com/sparta/java02/domain/user/dto/UserUpdateStatusRequest.java
@@ -1,5 +1,11 @@
 package com.sparta.java02.domain.user.dto;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserUpdateStatusRequest {
 
   String name;
